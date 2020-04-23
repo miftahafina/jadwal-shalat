efa6fceaffc9a0f51d1223dfa0567de69e186f2e
Miftah Afina <miftah.afina@gmail.com>
2020-04-23 18:56:40 +0700

    Change api, display all prayer times

--- src/App.js
+++ src/App.js
@@ -3,56 +3,57 @@ import Poi from './Icons/poi.png';
 import axios from 'axios';
 
 function App() {
-  // const [location, setLocation] = useState('Pemalang');
-  const [date, setDate] = useState(23);
-  const [data, setData] = useState([]);
+  const [location, setLocation] = useState('Pemalang');
+  const [date, setDate] = useState(new Date());
+  const [prayerTimes, setPrayerTimes] = useState([]);
 
   useEffect(() => {
-    axios.get('https://api.aladhan.com/v1/calendarByAddress?address=Pemalang,%20indonesia&month=04&year=2020&method=5')
+    const dateFormatted = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${date.getDate()}`
+    axios.get(`https://api.pray.zone/v2/times/day.json?city=${location}&date=${dateFormatted}&school=5`)
       .then((res) => {
-        console.log(res.data.data[date - 1].timings.Fajr);
-        setData(res.data.data[date - 1].timings.Fajr.replace(' (WIB)', ''));
+        console.log(res.data.results);
+        setPrayerTimes(res.data.results.datetime[0].times);
       });
     
-  }, [date])
+  }, [date, location])
 
   return (
   <div className="container h-screen p-4 text-center bg-gray-200 mx-auto">
 
     <div className="flex flex-row items-center justify-start px-4 bg-gray-400 p-3 rounded-full">
       <img className="h-6" src={Poi} alt="POI" />
-      <input type="text" className="bg-gray-400 mx-2 px-2 w-full text-base outline-none" name="location" id="location" value="Pemalang" />
+      <input type="text" className="bg-gray-400 mx-2 px-2 w-full text-base outline-none" name="location" id="location" value={location} />
     </div>
 
-    <div className="mt-20 text-6xl leading-none">04.24</div>
-    <div className="text-base">Rabu, 22 April 2020</div>
+  <div className="mt-20 text-6xl leading-none">{date.getHours()}.{date.getMinutes()}</div>
+    <div className="text-base">{date.getDate}</div>
 
     <div className="flex flex-row items-center space-x-4 justify-between mt-20">
       <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
-        <p className="text-2xl text-white">{data}</p>
+        <p className="text-2xl text-white">{prayerTimes.Fajr}</p>
         <small className="text-white">Shubuh</small>
       </div>
       <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
-        <p className="text-2xl text-white">17.49</p>
+        <p className="text-2xl text-white">{prayerTimes.Sunrise}</p>
         <small className="text-white">Terbit</small>
       </div>
       <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
-        <p className="text-2xl text-white">17.49</p>
+        <p className="text-2xl text-white">{prayerTimes.Dhuhr}</p>
         <small className="text-white">Dhuhur</small>
       </div>
     </div>
 
     <div className="flex flex-row items-center space-x-4 justify-between mt-4">
       <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
-        <p className="text-2xl text-white">17.49</p>
+        <p className="text-2xl text-white">{prayerTimes.Asr}</p>
         <small className="text-white">Ashar</small>
       </div>
       <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
-        <p className="text-2xl text-white">17.49</p>
+        <p className="text-2xl text-white">{prayerTimes.Maghrib}</p>
         <small className="text-white">Maghrib</small>
       </div>
       <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
-        <p className="text-2xl text-white">17.49</p>
+        <p className="text-2xl text-white">{prayerTimes.Isha}</p>
         <small className="text-white">Isya</small>
       </div>
     </div>
