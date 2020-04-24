5a37a638521282fe4189d2879599f7377fdc06be
Miftah Afina <miftah.afina@gmail.com>
2020-04-24 16:37:46 +0700

    Change color

--- src/App.js
+++ src/App.js
@@ -34,41 +34,41 @@ function App() {
   }
 
   return (
-  <div className="container h-screen p-4 text-center bg-gray-200 mx-auto">
+  <div className="container h-screen p-4 text-center bg-green-100 mx-auto">
 
-    <div className="flex flex-row items-center justify-start px-4 bg-gray-400 p-3 rounded-full">
+    <div className="flex flex-row items-center justify-start px-4 bg-green-500 p-3 rounded-full">
       <img className="h-6" src={Poi} alt="POI" />
-      <DebounceInput minLength="3" debounceTimeout={300} className="bg-gray-400 mx-2 px-2 w-full text-base outline-none" name="location" id="location" value={location} onChange={changeLocation} />
+      <DebounceInput minLength="3" debounceTimeout={300} className="bg-green-500 mx-2 px-2 w-full text-base outline-none" name="location" id="location" value={location} onChange={changeLocation} />
     </div>
 
     <div className="mt-20 text-6xl leading-none">{getDayIndo(date)}</div>
     <div className="text-base">{getDateIndo(date)}</div>
 
     <div className="flex flex-row items-center space-x-4 justify-between mt-20">
-      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
+      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-green-700 rounded-lg">
         <p className="text-2xl text-white">{prayerTimes.Fajr}</p>
         <small className="text-white">Subuh</small>
       </div>
-      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
+      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-green-700 rounded-lg">
         <p className="text-2xl text-white">{prayerTimes.Sunrise}</p>
         <small className="text-white">Terbit</small>
       </div>
-      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
+      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-green-700 rounded-lg">
         <p className="text-2xl text-white">{prayerTimes.Dhuhr}</p>
         <small className="text-white">Zuhur</small>
       </div>
     </div>
 
     <div className="flex flex-row items-center space-x-4 justify-between mt-4">
-      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
+      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-green-700 rounded-lg">
         <p className="text-2xl text-white">{prayerTimes.Asr}</p>
         <small className="text-white">Asar</small>
       </div>
-      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
+      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-green-700 rounded-lg">
         <p className="text-2xl text-white">{prayerTimes.Maghrib}</p>
         <small className="text-white">Magrib</small>
       </div>
-      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-gray-500 rounded-lg">
+      <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-green-700 rounded-lg">
         <p className="text-2xl text-white">{prayerTimes.Isha}</p>
         <small className="text-white">Isya</small>
       </div>
