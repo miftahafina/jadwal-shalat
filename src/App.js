5bbb5a898c740b657fe4cd25e6023a97d9e415c9
Miftah Afina <miftah.afina@gmail.com>
2020-04-26 13:24:59 +0700

    Check latitude

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import useGeolocation from 'react-hook-geolocation';
 
 import Container from './components/Container/Container';
 import SearchBox from './components/SearchBox/SearchBox';
@@ -14,8 +15,11 @@ function App() {
   const [dayIndo, setDayIndo] = useState('');
   const [dateIndo, setDateIndo] = useState('');
 
+  const geolocation = useGeolocation();
+
   useEffect(() => {
     const dateFormatted = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${date.getDate()}`
+    
     axios.get(`https://api.pray.zone/v2/times/day.json?city=${location}&date=${dateFormatted}&school=5`)
       .then((res) => {
         // console.log(res.data.results);
@@ -53,6 +57,11 @@ function App() {
 
       <div className="mt-20 text-6xl leading-none" onClick={nextDate}>{dayIndo}</div>
       <div className="text-base" onClick={nextDate}>{dateIndo}</div>
+      <p>
+        {
+          !geolocation.error ? <p>{geolocation.latitude}</p> : <p>geolocation error</p>
+        }
+      </p>
 
       <Table prayerTimes={prayerTimes}/>
       <Footer />
