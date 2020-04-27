5c3859705a8c7a9c643ce38880752d2c823bf45b
Miftah Afina <miftah.afina@gmail.com>
2020-04-27 11:11:56 +0700

    Change icon size

--- src/App.js
+++ src/App.js
@@ -88,17 +88,17 @@ function App() {
       <section className="block fixed inset-x-0 bottom-0 z-10 bg-gray-300 shadow">
         <div className="flex flex-row items-center justify-around">
           <a href="a" className="text-xs pt-2 pb-1 w-full bg-white text-center">
-            <img src={DiaryIcon} alt="Diary Icon" className="h-8 mb-2 mx-auto" />
+            <img src={DiaryIcon} alt="Diary Icon" className="w-6 mb-1 mx-auto" />
             Harian
           </a>
 
           <a href="b" className="text-xs pt-2 pb-1 w-full bg-white text-center">
-            <img src={CalendarIcon} alt="Calendar Icon" className="h-8 mb-2 mx-auto" />
+            <img src={CalendarIcon} alt="Calendar Icon" className="w-6 mb-1 mx-auto" />
             Pekanan
           </a>
           
           <a href="c" className="text-xs pt-2 pb-1 w-full bg-white text-center">
-            <img src={ForestIcon} alt="Forest Icon" className="h-8 mb-2 mx-auto" />
+            <img src={ForestIcon} alt="Forest Icon" className="w-6 mb-1 mx-auto" />
             Jelajah
           </a>
         </div>
