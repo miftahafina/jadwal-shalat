a62e39b8f69c54b8a5357a67f51d3ff596f2e539
Miftah Afina <miftah.afina@gmail.com>
2020-04-29 11:23:03 +0700

    Change indentation

--- src/Page/Monthly/Monthly.jsx
+++ src/Page/Monthly/Monthly.jsx
@@ -22,49 +22,52 @@ const Monthly = (props) => {
   }
 
   return (
-    <table className="table-auto w-full mt-8">
-      <thead>
-        <tr>
-          <th className="bg-gray-100 font-normal p-1 text-xs"></th>
-          <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs rounded-tl-lg">Subuh</th>
-          <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs">Zuhur</th>
-          <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs">Asar</th>
-          <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs">Magrib</th>
-          <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs rounded-tr-lg">Isya</th>
-        </tr>
-      </thead>
-      <tbody>
-      {
-        props.monthlyPrayerTimes.map((data, index) => {
-          return (
-            <Fragment key={index}>
-              {
-                new Date(data.date.gregorian).getDay() === 0 ?
-                <tr key={index + 'separator'}>
-                  <td></td>
-                  <td colSpan="5" className="text-xs text-gray-600">-</td>
+    <>
+      <h1 className="mt-6 leading-none">Jadwal Shalat</h1>
+      <table className="table-auto w-full my-6">
+        <thead>
+          <tr>
+            <th className="bg-gray-500 text-white border-2 border-gray-100 font-normal p-1 text-xs rounded-tl-lg">Tanggal</th>
+            <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs">Shubuh</th>
+            <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs">Dzuhur</th>
+            <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs">Ashar</th>
+            <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs">Maghrib</th>
+            <th className="bg-teal-500 text-white border-2 border-gray-100 font-normal p-1 text-xs rounded-tr-lg">Isya</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          props.monthlyPrayerTimes.map((data, index) => {
+            return (
+              <Fragment key={index}>
+                {
+                  new Date(data.date.gregorian).getDay() === 0 ?
+                  <tr key={index + 'separator'}>
+                    <td></td>
+                    <td colSpan="5" className="text-xs text-gray-600">-</td>
+                  </tr>
+                  : false
+                }
+                <tr>
+                  <td className={`p-1 text-xs text-center border-gray-100 text-gray-700 ${isToday(data.date.gregorian)}`}>
+                    {getDayPrefix(new Date(data.date.gregorian).getDay())}{', '}
+                    {new Date(data.date.gregorian).getDate()} {' '}
+                    {getMonthPrefix(new Date(data.date.gregorian).getMonth())}
+                  </td>
+                  <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Fajr}</td>
+                  <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Dhuhr}</td>
+                  <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Asr}</td>
+                  <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Maghrib}</td>
+                  <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Isha}</td>
                 </tr>
-                : false
-              }
-              <tr>
-                <td className={`p-1 text-xs text-center border-gray-100 text-gray-700 ${isToday(data.date.gregorian)}`}>
-                  {getDayPrefix(new Date(data.date.gregorian).getDay())}{', '}
-                  {new Date(data.date.gregorian).getDate()} {' '}
-                  {getMonthPrefix(new Date(data.date.gregorian).getMonth())}
-                </td>
-                <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Fajr}</td>
-                <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Dhuhr}</td>
-                <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Asr}</td>
-                <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Maghrib}</td>
-                <td className={`${isEven(index) ? 'bg-gray-300' : 'bg-gray-200'} border-x-2 border-gray-100 p-1 text-xs ${isToday(data.date.gregorian)}`}>{data.times.Isha}</td>
-              </tr>
-            </Fragment>
-            );
-          }
-        )
-      }
-      </tbody>
-    </table>
+              </Fragment>
+              );
+            }
+          )
+        }
+        </tbody>
+      </table>
+    </>
   );
 }
 
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -6,7 +6,7 @@ const Table = (props) => {
       <div className="flex flex-row items-center space-x-4 justify-between">
         <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-teal-500 text-white rounded-lg">
           <p className="text-2xl ">{props.prayerTimes.Fajr}</p>
-          <small>Subuh</small>
+          <small>Shubuh</small>
         </div>
         <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-teal-500 text-white rounded-lg">
           <p className="text-2xl ">{props.prayerTimes.Sunrise}</p>
@@ -14,18 +14,18 @@ const Table = (props) => {
         </div>
         <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-teal-500 text-white rounded-lg">
           <p className="text-2xl ">{props.prayerTimes.Dhuhr}</p>
-          <small>Zuhur</small>
+          <small>Dzuhur</small>
         </div>
       </div>
 
       <div className="flex flex-row items-center space-x-4 justify-between mt-4">
         <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-teal-500 text-white rounded-lg">
           <p className="text-2xl ">{props.prayerTimes.Asr}</p>
-          <small>Asar</small>
+          <small>Ashar</small>
         </div>
         <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-teal-500 text-white rounded-lg">
           <p className="text-2xl ">{props.prayerTimes.Maghrib}</p>
-          <small>Magrib</small>
+          <small>Maghrib</small>
         </div>
         <div className="flex flex-col items-center justify-center h-20 px-2 w-1/3 bg-teal-500 text-white rounded-lg">
           <p className="text-2xl ">{props.prayerTimes.Isha}</p>
