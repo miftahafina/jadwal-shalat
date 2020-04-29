36234cbca958d50af15468ce2ac2eb8412077a98
Miftah Afina <miftah.afina@gmail.com>
2020-04-29 12:14:59 +0700

    Change onclick from img to button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="text-xs mt-8 text-gray-700">
+  <footer className="text-xs mt-8 mb-20 text-gray-700">
     Data diperoleh dari API <a href="https://prayertimes.date/api/">Prayer Times</a><br/>
     dengan penyesuaian mengikuti <a href="https://bimasislam.kemenag.go.id/jadwalshalat">Kemenag RI</a>.
   </footer>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -15,18 +15,18 @@ const Nav = () => {
   return (
     <section className="block fixed inset-x-0 bottom-0 z-10 bg-gray-300 shadow">
       <div className="flex flex-row items-center justify-around">
-        <button className="text-xs pt-2 pb-1 w-full bg-white text-center">
-          <img src={DiaryIcon} alt="Diary Icon" className="w-6 mb-1 mx-auto" onClick={() => handleClick('/')} />
+        <button className="text-xs pt-2 pb-1 w-full bg-white text-center" onClick={() => handleClick('/')}>
+          <img src={DiaryIcon} alt="Diary Icon" className="w-6 mb-1 mx-auto" />
           Harian
         </button>
 
-        <button className="text-xs pt-2 pb-1 w-full bg-white text-center">
-          <img src={CalendarIcon} alt="Calendar Icon" className="w-6 mb-1 mx-auto" onClick={() => handleClick('/monthly')} />
+        <button className="text-xs pt-2 pb-1 w-full bg-white text-center" onClick={() => handleClick('/monthly')}>
+          <img src={CalendarIcon} alt="Calendar Icon" className="w-6 mb-1 mx-auto" />
           Bulanan
         </button>
         
-        <button className="text-xs pt-2 pb-1 w-full bg-white text-center">
-          <img src={ForestIcon} alt="Forest Icon" className="w-6 mb-1 mx-auto" onClick={() => handleClick('/')} />
+        <button className="text-xs pt-2 pb-1 w-full bg-white text-center" onClick={() => handleClick('/')}>
+          <img src={ForestIcon} alt="Forest Icon" className="w-6 mb-1 mx-auto" />
           Jelajah
         </button>
       </div>
